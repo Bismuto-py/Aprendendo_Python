7b590e35d7dbcb78a3f5f7ea8319e9c2ab4f4c6b
Julia Hevellyn de Melo Fidelis <20261016270013@ifrn.local>
2026-05-05 21:50:02 -0300

    Ajuste

---
+++ aula5.py
@@ -0,0 +1,11 @@
+# contador = 1
+# while contador>= 3: #para incertezas, cuidado com o loop. Deve ter condição e encrementação
+#     print(f"Tentativa {contador}")
+#     contador = contador + 1 #ou contador += 1
+
+# for i  in range (10):
+
+for i in range (1, 11):
+    mult = 5 * i
+    print (f"5 x {i} = {mult}")
+

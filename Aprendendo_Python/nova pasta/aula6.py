614332d9e9ae387e073e9c91bafb7d9daa489305
Julia <juliahevellyndesign@gmail.com>
2026-05-06 21:39:26 -0300

    Add files via upload

---
+++ Aprendendo_Python/nova pasta/aula6.py
@@ -0,0 +1,27 @@
+# frutas = ["maçã", "banana", "laranja"]
+# # for fruta in frutas:
+# #     print(fruta)
+
+# #Método
+
+# frutas.pop(1)
+
+# print(frutas)
+
+# frutas.append("uva")
+# print(frutas)
+
+# frutas.remove("banana")
+# print(frutas)
+
+# frutas.insert(1, "abacaxi")
+# print(frutas)
+
+alunas = ["Maria", "Joana", "Ana"]
+print(alunas)
+
+alunas.append("Helena")
+print(alunas) 
+
+alunas.pop(1)
+print(alunas)
